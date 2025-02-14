b362400d69be3bb95a791ddc4c4899101bde5224
athing <pengweiming@bnu.edu.cn>
2025-02-14 09:13:59 -0500

    init search module

--- duanzhu_proj/manuscript/urls.py
+++ duanzhu_proj/manuscript/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import catalogue_data, zitou_detail, index
+from .views import catalogue_data, zitou_detail, index, search
 
 urlpatterns = [
     path('', index, name='index'),
     path('catalogue/', catalogue_data, name='catalogue'),
     path('zitou/<uuid:zitou_id>/', zitou_detail, name='zitou_detail'),
+    path('search/', search, name='search'),
+
 ]
