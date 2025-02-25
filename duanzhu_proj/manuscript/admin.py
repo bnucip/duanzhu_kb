78aabe4f1cf985ee9ac2961b8417e27cb7275402
pengzl <393290508@qq.com>
2025-02-25 22:12:40 +0800

    update

--- duanzhu_proj/manuscript/admin.py
+++ duanzhu_proj/manuscript/admin.py
@@ -13,24 +13,24 @@ class DuanZhuAdmin(admin.ModelAdmin):
 @admin.register(SwDu)
 class SwDuZhuAdmin(admin.ModelAdmin):
     list_display = ('du_bianhao', 'zitou', 'zhushi', 'duanzhu_bianhao')  # Customize the admin list view
-    search_fields = ('zitou',)  # Enable search functionality
+    search_fields = ('zitou','du_bianhao', 'zitou', 'zhushi', 'duanzhu_bianhao')  # Enable search functionality
 
-# @admin.register(Xiesheng)
-# class XieshengAdmin(admin.ModelAdmin):
-#     list_display = ('xieshengzi', 'shengfu')  # Customize the admin list view
-#     search_fields = ('xieshengzi', 'shengfu',)
+@admin.register(Xiesheng)
+class XieshengAdmin(admin.ModelAdmin):
+    list_display = ('xieshengzi', 'shengfu')  # Customize the admin list view
+    search_fields = ('xieshengzi', 'shengfu',)
 
 @admin.register(Guyunbu)
 class GuyunbuAdmin(admin.ModelAdmin):
     list_display = ('duanzhu_bianhao', 'zitou', 'yunbu', 'yunlei')  # Customize the admin list view
-    search_fields = ('zitou',)
+    search_fields = ('zitou','duanzhu_bianhao', 'zitou', 'yunbu', 'yunlei')
 
 @admin.register(Gouyi)
 class GouyiAdmin(admin.ModelAdmin):
     list_display = ('duanzhu_bianhao', 'gouyi1', 'gouyi2', 'gouyi3','shengfugouyi')  # Customize the admin list view
-    search_fields = ('gouyi1', 'gouyi2', 'gouyi3','shengfugouyi',)
+    search_fields = ('duanzhu_bianhao','gouyi1', 'gouyi2', 'gouyi3','shengfugouyi',)
 
 @admin.register(Gujinzi)
 class GujinziAdmin(admin.ModelAdmin):
     list_display = ('duanzhu_bianhao', 'chuxian_zitou', 'miaoshu', 'guzi_id','jinzi_id')  # Customize the admin list view
-    search_fields = ('chuxian_zitou', 'miaoshu',)
+    search_fields = ('duanzhu_bianhao','chuxian_zitou', 'miaoshu','guzi_id','jinzi_id')
